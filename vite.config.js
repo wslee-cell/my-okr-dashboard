e79bcc9b1094e881212d7d125a8c9f26d8506c0a
이우석 <ws.lee@olit.co.kr>
2025-11-10 22:54:39 +0900

    PostCSS 설정을 vite.config.js로 통합 (최종 해결)

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from 'tailwindcss' // <-- 1. 디자인 도구(tailwind) 불러오기
+import autoprefixer from 'autoprefixer' // <-- 2. 디자인 도구(autoprefixer) 불러오기
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  // 3. CSS 설정을 여기에 '통합'하기
+  css: {
+    postcss: {
+      plugins: [tailwindcss, autoprefixer],
+    },
+  },
   plugins: [react()],
 })
